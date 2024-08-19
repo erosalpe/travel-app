8a4c027f5b028443bcaa67d275630801d429459e
erosalpe <eros.alpegiani@gmail.com>
2024-08-19 16:23:28 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
